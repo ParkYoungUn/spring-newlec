c6bdd2e7ce83892d67ebc1f3bdc1ad3cc69da5b8
User <User@DESKTOP-VFTMQ9S>
2020-09-21 23:47:43 +0900

    2020/09/21 뉴렉처 스프링 16강

--- src/spring/di/Program.java
+++ src/spring/di/Program.java
@@ -27,19 +27,18 @@ public class Program {
 		
 		ApplicationContext context = 
 				new ClassPathXmlApplicationContext("spring/di/setting.xml");
-		Exam exam = context.getBean(Exam.class);
-		System.out.println(exam.toString());
+//		Exam exam = context.getBean(Exam.class);
+//		System.out.println(exam.toString());
 		ExamConsole console = (ExamConsole) context.getBean("console");
 //		ExamConsole console = context.getBean(ExamConsole.class); // 위 방법보다는 선호되는 방법
-		
 		console.print();
 		
-		List<Exam> exams = (List<Exam>) context.getBean("exams");	//new ArrayList<>();
-		exams.add(new NewlecExam(1,1,1,1));
+//		List<Exam> exams = (List<Exam>) context.getBean("exams");	//new ArrayList<>();
+//		exams.add(new NewlecExam(1,1,1,1));
 		
 		
-		for(Exam e : exams)
-			System.out.println(e);
+//		for(Exam e : exams)
+//			System.out.println(e);
 	}
 
 }
--- src/spring/di/ui/InlineExamConsole.java
+++ src/spring/di/ui/InlineExamConsole.java
@@ -1,16 +1,24 @@
 package spring.di.ui;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
 import spring.di.entity.Exam;
 
+@Component("console")
 public class InlineExamConsole implements ExamConsole {
-
+	// 일반적으로 선호되는 위치
+	@Autowired(required = true)	// 기본생성자를 생성하면서 인젝션 됨
+	//@Qualifier("exam2")	// 객체가 2개이상 있어서 식별이 모호할 경우 선택할수 있다.
 	private Exam exam;
 	
 	public InlineExamConsole() {
-		// TODO Auto-generated constructor stub
+		System.out.println("constructor");
 	}
 	
 	public InlineExamConsole(Exam exam) {
+		System.out.println("overloaded constructor");
 		this.exam = exam;
 	}
 
@@ -18,11 +26,17 @@ public class InlineExamConsole implements ExamConsole {
 
 	@Override
 	public void print() {
+		if (exam == null)
+			System.out.printf("total is %d avg is %f\n", 0 , 0.0);
+		else
 		System.out.printf("total is %d avg is %f\n",exam.total(),exam.avg());
 	}
 
+//	@Autowired	// 자동으로 객체를 연결
+//	@Qualifier("exam2")	// 객체가 2개이상 있어서 식별이 모호할 경우 선택할수 있다.
 	@Override
 	public void setExam(Exam exam) {
+		System.out.println("setter");
 		this.exam = exam;
 	}
 }
